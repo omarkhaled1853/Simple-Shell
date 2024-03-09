7ec03c417d90dfeccd5344bf309740bde3538f2b
omar <omar@MyUbuntu.myguest.virtualbox.org>
2024-03-10 01:18:01 +0200

    adding all remeaning functions and some modifications

--- simple_shell.c
+++ simple_shell.c
@@ -4,39 +4,107 @@
 #include <stdbool.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <signal.h>
 
 int max_words = 20, idx = 0;
-char line[100] = {};
-char *args[20] = {};
-char variables[100][100] = {};
-char values[100][100] = {};
+char line[100];
+char **args = NULL;
+char variables[100][100];
+char values[100][100];
+char *filepath = "logfile.txt";
 
 // get input line from user
 void parse_input(){
     fgets(line, sizeof(line), stdin);
+    int length = strlen(line);
+    if (line[length - 1] == '\n') {
+        line[length - 1] = '\0';
+    }
+}
+
+void replace_variable() {
+char *dollar_sign = strchr(line, '$');
+    if (dollar_sign != NULL) {
+        // Find the position of the first space after '$'
+        char *space_after_dollar = dollar_sign + 1;
+        while (*space_after_dollar != ' ' && *space_after_dollar != '\0') {
+            space_after_dollar++;
+        }
+        
+
+        // Calculate the length of the variable name (after '$' until the first space)
+        int variable_length = space_after_dollar - dollar_sign - 1;
+
+        // Copy the variable name (after '$' until the first space)
+        char variable_name[100];
+        strncpy(variable_name, dollar_sign + 1, variable_length);
+        variable_name[variable_length] = '\0';
+
+        // Search for the variable in variables array
+        for (int i = 0; i < 100 && variables[i][0] != '\0'; i++) {
+            if (strcmp(variables[i], variable_name) == 0) {
+                // Calculate the length of the variable value
+                int value_length = strlen(values[i]);
+                
+                // Calculate the length of the remaining part after the variable name
+                int remaining_length = strlen(dollar_sign + variable_length + 1);
+
+                // If the value is longer than the variable name, we need to shift the remaining part
+                if (value_length > variable_length) {
+                    // Shift the remaining part to make space for the value
+                    memmove(dollar_sign + value_length, dollar_sign + variable_length + 1, remaining_length + 1);
+                }
+                strncpy(dollar_sign, values[i], value_length);
+                break;
+            }
+        }
+    }
 }
 
 // splite input line in the spaces
 void evaluate_expression(){
+    replace_variable();
+    printf("%s\n", line);
+    args = malloc(max_words * sizeof(char*));
+    if (args == NULL) {
+        printf("Memory allocation failed!\n");
+        exit(1);
+    }
+
     char tmp[100];
     int i = 0;
     while(line[i] != '\0'){
         tmp[i] = line[i];
         i++;
     }
+    tmp[i] = '\0';
     char *token = strtok(tmp, " \n");
     i = 0;
     while (token != NULL && i < max_words - 1) {
-        args[i] = token;
+        args[i] = strdup(token);
+        if (args[i] == NULL) {
+            printf("Memory allocation failed!\n");
+            exit(1);
+        }
         i++;
         token = strtok(NULL, " \n");
     }
-    args[i] = NULL;   
+    args[i] = NULL;
 }
 
 // check if builtin shell 
 bool shell_builtin(){
     return strcmp(args[0], "cd") == 0 || strcmp(args[0], "echo") == 0 || strcmp(args[0], "export") == 0;
+}
+
+// check for zombie process
+bool check_background_process(){
+    for(int i = 0; args[i] != NULL; i++){
+        if(strcmp(args[i], "&") == 0){
+            return true;
+        }
+    }
+    return false;
 }
 
 // print current working directory
@@ -49,14 +117,29 @@ void cwd(){
         exit(0);
     }
     else{
-        wait(&id);
+        waitpid(id, NULL, 0);
     }
 }
 
-// function on_child_exit()
-//     reap_child_zombie()
-//     write_to_log_file("Child terminated")
+// kill zombies and write in file
+void on_child_exit(int sig){
+    int status;
+    pid_t pid;
 
+    while((pid = waitpid(-1, &status, WNOHANG)) > 0){
+        FILE *fptr = fopen(filepath, "a");
+        if(fptr != NULL){
+            printf("done \n");
+            fprintf(fptr, "child terminated\n");
+            fclose(fptr);
+        }
+        else {
+            fprintf(stderr, "Error opening file\n");
+        }
+        cwd();
+    }
+    
+}
 
 void cd(){
     if(args[1] == NULL || strcmp(args[1], "~") == 0){
@@ -77,13 +160,21 @@ void cd(){
         chdir(args[1]);
     }
 }
-
+// printig what inside quotes
 void echo(){
     int id = fork();
     if(id == 0){
         const char *start = strchr(line, '"');
+        if(start == NULL){
+            printf("Not found starting quote\n");
+            exit(0);
+        }
         start++;
         const char *end = strchr(start, '"');
+        if(end == NULL){
+            printf("Not found ending quote\n");
+            exit(0);
+        }
         for (const char *ptr = start; ptr < end; ptr++) {
             putchar(*ptr);
         }
@@ -91,26 +182,38 @@ void echo(){
         exit(0);
     }
     else{
-        wait(&id);
+        waitpid(id, NULL, 0);
     }
 }
 
+// making variables
 void export(){
-    // int id = fork();
-    // if(id == 0){
-        const char *equal = strchr(args[1], '=');
-        strncpy(variables[idx], args[1], equal - args[1]);
-        variables[idx][equal - args[1]] = '\0';
-        strcpy(values[idx], equal + 1);
-        printf("%s\n", variables[idx]);
-        printf("%s\n", values[idx]);
-        idx++;
-    //     exit(0);
-    // }
-    // else{
-    //     wait(&id);
-    // }
+    // Find the position of '='
+    const char *equal_sign = strchr(line, '=');
+    if (equal_sign == NULL) {
+        printf("Invalid export string\n");
+        return;
+    }
 
+    // Find the position of the first space before '='
+    const char *space_before_equal = equal_sign;
+    while (space_before_equal > line && *(space_before_equal - 1) != ' ') {
+        space_before_equal--;
+    }
+
+    // Copy the variable name (before '=' until the first space)
+    int variable_length = equal_sign - space_before_equal;
+    strncpy(variables[idx], space_before_equal, variable_length);
+    variables[idx][variable_length] = '\0';
+
+    // Copy the value (after '=')
+    strcpy(values[idx], equal_sign + 1);
+    int value_length = strlen(values[idx]);
+    if (values[idx][value_length - 1] == '\n') {
+        values[idx][value_length - 1] = '\0';
+    }
+    // printf("%s", values[idx]);
+    idx++;
 }
 
 void execute_shell_bultin(){
@@ -125,16 +228,26 @@ void execute_shell_bultin(){
     }
     
 }
+
 // execute non builtin shell 
 void execute_command(){
     int id = fork();
-    if (id == 0){
-        execvp(args[0], args);
-        printf("Error\n");
-        exit(0);
+    if(check_background_process()){
+        if (id == 0){
+            execvp(args[0], args);
+            printf("command not found\n");
+            exit(0);
+        }
     }
     else{
-        wait(&id);
+        if (id == 0){
+            execvp(args[0], args);
+            printf("command not found\n");
+            exit(0);
+        }
+        else{
+            waitpid(id, NULL, 0);
+        }
     }
 }
 
@@ -142,7 +255,7 @@ void shell(){
     do{
         cwd();
         parse_input();
-        evaluate_expression();    
+        evaluate_expression();      
         if(shell_builtin()){
             execute_shell_bultin();
         }
@@ -160,7 +273,7 @@ void setup_environment(){
 }
 
 int main(){
-    // register_child_signal(on_child_exit())
+    signal(SIGCHLD, on_child_exit);
     setup_environment();
     shell();
     return 0;
